f8e8332baf8a1932aad87b016dc804bf3f776132
kimothokamau <kimothokamau@gmail.com>
2021-08-24 10:35:51 -0600

    Add props inheritance from parent to children

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+import TodoContainer from './components/TodoContainer';
+
+ReactDOM.render(
+  <TodoContainer />,
+  document.getElementById('root'),
+);
