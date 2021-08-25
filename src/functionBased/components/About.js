f625cabc64c46e8b2c24f6590b7374261920eeec
kimothokamau <kimothokamau@gmail.com>
2021-08-25 09:07:36 -0600

    Add nested routing part1

--- src/functionBased/components/About.js
+++ src/functionBased/components/About.js
@@ -1,9 +1,25 @@
 import React from "react"
-import { useRouteMatch } from "react-router-dom"
+import { Link, useRouteMatch, Route } from "react-router-dom"
+import SinglePage from "./SinglePage"
 
 
 const About = (props) => {
-  console.log(useRouteMatch())
-  return <div>hello from about page</div>
+  const { url, path } = useRouteMatch()
+  return (
+      <div>
+        <ul>
+          <li>
+            <Link to={`${url}/about-app`}>About App</Link>
+          </li>
+          <li>
+            <Link to={`${url}/about-author`}>About Author</Link>
+          </li>
+        </ul>
+        <Route path={`${path}/:slug`}>
+          <SinglePage />
+        </Route>
+      </div>
+  )
 }
+
 export default About
